59beb2888f08e09c86f670f0cb1005332d97e4f0
root <uchenna333@yahoo.com>
2023-06-16 10:04:08 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long d;
 long long e;
 float g;
-printf("Size of char:%zu byte(s)\n", sizeof(a));
-printf("Size of int:%zu byte(s)\n", sizeof(b));
-printf("Size of long int:%zu byte(s)\n", sizeof(d));
-printf("Size of long long byte:%zu byte(s)\n", sizeof(e));
-printf("Size of float:%zu byte(s)\n", sizeof(g));
+printf("Size of a char: %zu byte(s)\n", sizeof(a));
+printf("Size of an int: %zu byte(s)\n", sizeof(b));
+printf("Size of a long int: %zu byte(s)\n", sizeof(d));
+printf("Size of a long long byte: %zu byte(s)\n", sizeof(e));
+printf("Size of a float: %zu byte(s)\n", sizeof(g));
 return (0);
 }
