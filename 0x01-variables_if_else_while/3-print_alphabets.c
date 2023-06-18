a6cb61366f0cac0e03442863cb52c6c686523938
root <uchenna333@yahoo.com>
2023-06-18 11:53:41 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char n;
+for (n = 97; n < 123; n++)
+{
+	putchar(n);
+	}
+for (n = 65; n < 91; n++)
+{
+	putchar(n);
+}
+putchar('\n');
+return (0);
+}
