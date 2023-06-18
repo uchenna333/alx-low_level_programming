88fa7f28d4434146c3c8f6a791a1240bc4b821a5
root <uchenna333@yahoo.com>
2023-06-18 16:47:12 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char n;
+for (n = 122; n >= 97; n--)
+{
+	putchar(n);
+	}
+putchar('\n');
+return (0);
+}
