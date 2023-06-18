5fba943a65e458fdf3d2c858deb1473f618d8c5a
root <uchenna333@yahoo.com>
2023-06-18 01:55:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char n;
+for (n = 97; n < 123; n++)
+{
+	putchar(n);
+	}
+putchar('\n');
+return (0);
+}
