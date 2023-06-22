8f0cb8497788a44862222c569667bec3dcf6c2bc
root <uchenna333@yahoo.com>
2023-06-21 21:17:42 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -2,9 +2,9 @@
 
 /**
  * main - check the code.
+ *
  * Return: Always 0.
  */
-
 int main(void)
 {
     int r;
@@ -30,3 +30,4 @@ int main(void)
     _putchar(r + '0');
     _putchar('\n');
     return (0);
+}
