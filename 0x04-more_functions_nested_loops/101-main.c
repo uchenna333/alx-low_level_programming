0004fa27da573b6752b323417e93bcdd8a8b5b4b
root <uchenna333@yahoo.com>
2023-06-22 18:13:29 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -5,17 +5,19 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    return (0);
+print_number(98);
+_putchar('\n');
+print_number(402);
+_putchar('\n');
+print_number(1024);
+_putchar('\n');
+print_number(0);
+_putchar('\n');
+print_number(-98);
+_putchar('\n');
+print_numbers(98);
+return (0);
 }
